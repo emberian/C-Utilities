e7a62f977d4bca4ef55bceccfdca2a27ffdaf6a8
Corey Richardson <corey@octayn.net>
2012-10-10 10:35:56 -0400

    Add Array documentation
    The documentation isn't entirely complete yet, and during the process of
    documentation I found a few unresolved issues; PR later.

--- Array.c
+++ Array.c
@@ -1,5 +1,16 @@
+/**
+ * @file Array.c
+ * @brief A dynamicly sized array that expands as you append, but remains
+ * contiguous.
+ */
 #include "Array.h"
 
+/**
+ * Create a new array.
+ *
+ * @param size Size in bytes of new array
+ * @returns pointer to newly initialzed array
+ */
 Array* Array_New(uint64 size) {
     Array* array;
     
@@ -9,6 +20,13 @@ Array* Array_New(uint64 size) {
     return array;
 }
 
+/**
+ * Create a new array using data from an existing buffer.
+ *
+ * @param data Pointer to buffer to copy data from
+ * @param size New size of array
+ * @returns pointer to newly created array
+ */
 Array* Array_NewFromExisting(uint8* data, uint64 size) {
     Array* array;
     uint64 actualSize;
@@ -23,11 +41,17 @@ Array* Array_NewFromExisting(uint8* data, uint64 size) {
     array = Allocate(Array);
     array->Size = size;
     array->Allocation = actualSize;
-    array->Data = ReallocateArray(uint8, actualSize, data);
+    array->Data = ReallocateArray(uint8, actualSize, data); // XXX: copy?
 
     return array;
 }
 
+/**
+ * Initialize an already allocated array.
+ *
+ * @param array array object to initialize
+ * @param size size of new array
+ */
 void Array_Initialize(Array* array, uint64 size) {
     uint64 actualSize;
 
@@ -42,6 +66,12 @@ void Array_Initialize(Array* array, uint64 size) {
     array->Data = AllocateArray(uint8, actualSize);
 }
 
+/**
+ * Free the data + deinitialize an array.
+ *
+ * @param self Array to dispose of.
+ * @warning @a self is not freed!
+ */
 void Array_Dispose(Array* self) {
     assert(self != NULL);
 
@@ -50,6 +80,12 @@ void Array_Dispose(Array* self) {
     self->Allocation = 0;
 }
 
+/**
+ * Resize an array.
+ *
+ * @param self Array to resize
+ * @param newSize Desired size of array
+ */
 void Array_Resize(Array* self, uint64 newSize) {
     uint64 actualSize;
 
@@ -65,6 +101,13 @@ void Array_Resize(Array* self, uint64 newSize) {
     self->Size = newSize;
 }
 
+/**
+ * Read bytes from an array.
+ *
+ * @param self Array to read from
+ * @param position Offset (in bytes) from start of the array to read from
+ * @param amount Number of bytes to read
+ */
 uint8* Array_Read(Array* self, uint64 position, uint64 amount) {
     assert(self != NULL);
     assert(position + amount <= self->Size);
@@ -72,6 +115,14 @@ uint8* Array_Read(Array* self, uint64 position, uint64 amount) {
     return self->Data + position;
 }
 
+/**
+ * Write data to an array, resizing if needed.
+ *
+ * @param self Array to write to
+ * @param position Offset (in bytes) from start of array to write to
+ * @param data Data to write
+ * @param amount Number of bytes to write from @a data
+ */
 void Array_Write(Array* self, const uint8* data, uint64 position, uint64 amount) {
     uint8 i;
 
@@ -83,6 +134,12 @@ void Array_Write(Array* self, const uint8* data, uint64 position, uint64 amount)
         *(self->Data + position + i) = *(data + i);
 }
 
+/**
+ * Append an array to another array.
+ *
+ * @param self Array to append to
+ * @param source Array to append from
+ */
 void Array_Append(Array* self, Array* source) {
     assert(self != NULL && source != NULL);
 
